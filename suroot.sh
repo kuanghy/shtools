99b29f1b280f2852b770da9423bb55152c6dbcc2
Huayong Kuang <sudohuoty@163.com>
2016-06-20 14:07:46 +0800

    add suroot

---
+++ suroot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author: huoty <sudohuoty@gmail.com>
+#   Date: 2016.05.10
+
+# Script starts from here
+
+ROOT_PASSWD="123456"
+
+expect -c "
+spawn /bin/su root
+expect \"*?password:*\"
+send -- \"$ROOT_PASSWD\r\"
+send -- \"\r\"
+interact
+"
+
+exit 0
+
