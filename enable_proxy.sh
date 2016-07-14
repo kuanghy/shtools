c4dd731d7d3d5f0269992668b29f8b9b64468331
Huayong Kuang <sudohuoty@163.com>
2016-07-14 14:55:53 +0800

    add enproxy

---
+++ enable_proxy.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Filename: enable_proxy.sh 2016-07-14
+# Author: Huoty <sudohuoty@gmail.com>
+# Script starts from here:
+
+proxy=offlinehost:58892
+
+export http_proxy=$proxy
+export https_proxy=$proxy
+export HTTP_PROXY=$proxy
+export HTTPS_PROXY=$proxy
+
+exec $*
+
