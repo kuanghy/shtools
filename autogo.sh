b50f73d36e11538e48bce5c1f3f4ba84c7e190a9
Huayong Kuang <sudohuoty@163.com>
2016-12-08 15:29:32 +0800

    登录脚本使用 ssh-key 或者手动密码的方式

---
+++ autogo.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Author: huoty <sudohuoty@gmail.com>
+#   Date: 2016.09.18
+
+# Script starts from here
+
+if [ $# -ne 1 ]; then
+    echo "Usage: relogin <host>"
+    exit 0
+fi
+
+if [ ! -d ~/.autossh ]; then
+    mkdir -p ~/.autossh
+fi
+
+USER_AUTOSSH_DIR="$HOME/.autossh"
+
+if [ -s $USER_AUTOSSH_DIR/autossh.log ]; then
+    TEN_MBYTE=`expr 10 \* 1024 \* 1024`
+    logsize=`du -b $USER_AUTOSSH_DIR/autossh.log | awk '{ print $1 }'`
+    if [ "$logsize" -gt "$TEN_MBYTE" ]; then
+        timestamp=`date +%Y``date +%m``date +%d``date +%H``date +%M``date +%S`
+        (
+            cd $USER_AUTOSSH_DIR
+            tar czvf autossh_${timestamp}.log.tar.gz autossh.log 
+            rm -rf $USER_AUTOSSH_DIR/autossh.log
+        ) > /dev/null
+    fi
+fi
+
+export AUTOSSH_PIDFILE="$USER_AUTOSSH_DIR/autossh.pid"
+export AUTOSSH_LOGFILE="$USER_AUTOSSH_DIR/autossh.log"
+export AUTOSSH_FIRST_POLL=30
+export AUTOSSH_POLL=60
+export AUTOSSH_GATETIME=0
+export AUTOSSH_DEBUG=1
+
+random() {
+    while true; do
+        num=$RANDOM
+        if [ "$num" -gt 20000 ]; then
+            echo $num
+            break
+        fi
+    done
+}
+
+case $1 in 
+    offline)
+        USER="huayong"
+        HOST="offlinehost"
+        PORT="23456"
+        ;;
+    research)
+        USER="server"
+        HOST="researchhost"
+        PORT="22"
+        ;;
+    vultr)
+        USER="huoty"
+        HOST="vultrhost"
+        PORT="22"
+        ;;
+    *)
+        echo "$1 does not exist"
+        exit 0
+        ;;
+esac
+
+exec autossh -M $(random) $USER@$HOST -p $PORT
+
