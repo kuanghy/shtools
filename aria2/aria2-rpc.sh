03550579f8b4975feb088615e390ede2e1d1fbb7
Huayong Kuang <sudohuoty@163.com>
2018-08-24 21:56:03 +0800

    add aria2

---
+++ aria2/aria2-rpc.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Author: Huoty <sudohuoty@gmail.com>
+# CreateTime: 2018-08-24 21:54:22
+# Script starts from here:
+
+# Aria2 RPC #
+/usr/local/bin/aria2c --enable-rpc=true --input-file=/Users/name/.aria2/aria2.session --conf-path=/Users/name/.aria2/aria2.conf
