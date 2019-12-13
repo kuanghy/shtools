45fa5cc83c634a9297aabd4a8eb2ffe696f7b60f
Huayong Kuang <kuanghuayong@joinquant.com>
2019-12-13 16:25:28 +0800

    add script

---
+++ list-starting-items-in-maxosx.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Author: Huoty <sudohuoty@163.com>
+# CreateTime: 2019-12-13 16:15:06
+# Script starts from here:
+
+find /System/Library/LaunchDaemons -type f
+find /Library/LaunchDaemons -type f
+find /System/Library/LaunchAgents -type f
+find /Library/LaunchAgents -type f
+find /System/Library/StartupItems -type f
+find /Library/StartupItems -type f
+find ~/Library/LaunchAgents -type f
+# find ~/Library/LaunchDaemons -type f
