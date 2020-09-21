66fd1e2baa5a6f930365ec5fc2425a5042a1d505
Huayong Kuang <sudohuoty@163.com>
2020-09-21 09:46:30 +0800

    update tmux init script

--- tmux/tmux-init.sh
+++ tmux/tmux-init.sh
@@ -8,14 +8,16 @@ set -e
 
 tmux_init()
 {
-    tmux new-session -s "huoty" -d -n "local"    # 开启一个会话
-    tmux new-window -n "other"          # 开启一个新窗口
-    tmux select-window -t "local"       # 切换窗口到 local
-    tmux -2 attach-session -d           # tmux -2 强制启用 256color，连接已开启的 tmux
+    tmux new-session -s "work" -d -n "local"     # 新建一个后台会话
+    tmux new-window -n "other"                   # 开启一个新窗口
+    tmux new-window -n                           # 再开启一个新窗口
+    tmux select-window -t "local"                # 切换窗口到 local
+
+    # 连接已开启的 tmux 会话, 参数-2 表示强制启用 256color 支持
+    tmux -2 attach-session -d
 }
 
 # 判断是否已有开启的tmux会话，没有则开启
 if which tmux 2>&1 >/dev/null; then
     test -z "$TMUX" && (tmux attach || tmux_init)
 fi
-
