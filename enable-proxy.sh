054e3deba93a65a279f40318216edb3f7f451a0f
Huayong Kuang <sudohuoty@163.com>
2021-07-31 23:59:46 +0800

    update script enable-proxy

--- enable-proxy.sh
+++ enable-proxy.sh
@@ -4,12 +4,19 @@
 # Author: Huoty <sudohuoty@gmail.com>
 # Script starts from here:
 
-proxy=http://localhost:12380
+proxy=http://127.0.0.1:12380
 
 export http_proxy=$proxy
 export https_proxy=$proxy
 export HTTP_PROXY=$proxy
 export HTTPS_PROXY=$proxy
+export ftp_proxy=$proxy
+export FTP_PROXY=$proxy
+
+export all_proxy=socks://127.0.0.1:12389
+export ALL_PROXY=$all_proxy
+
+export no_proxy=localhost,127.0.0.0/8,::1
+export NO_PROXY=$no_proxy
 
 exec $*
-
