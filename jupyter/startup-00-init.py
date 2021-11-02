cf49f42a3c3280bdc7ebe5494019709cbc4a082e
Huayong Kuang <sudohuoty@163.com>
2021-11-02 18:42:47 +0800

    update jupyter startup script

--- jupyter/startup-00-init.py
+++ jupyter/startup-00-init.py
@@ -5,11 +5,17 @@
 # Author: Huoty <sudohuoty@163.com>
 # CreateTime: 2021-02-25 13:08:25
 
-try:
-    import pandas as pd
-    import numpy as np
+def _import_module(name):
+    try:
+        return __import__(name)
+    except ImportError:
+        return None
 
+
+pd = pandas = _import_module("pandas")
+np = numpy = _import_module("numpy")
+
+
+if pandas:
     pd.set_option('precision', 12)
     pd.set_option('display.max_rows', 500)
-except Exception:
-    pass
